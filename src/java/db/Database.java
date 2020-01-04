106bab6b12839d6e46528ceeae6e989ad44ae461
Hadi Farhat <hadyfarhat97@gmail.com>
2020-01-04 19:44:03 +0000

    get shares data from database and put them in a hashmap

--- src/java/db/Database.java
+++ src/java/db/Database.java
@@ -5,11 +5,12 @@
  */
 package db;
 
-import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 
 
 /**
@@ -20,7 +21,11 @@ public class Database {
     public Database() {
     }
     
-    public String createShareTableSQLStatement() {
+    /**
+     * Builds up an SQL string that will be used to create the Shares Table
+     * @return SQL String
+     */
+    private String createShareTableSQLStatement() {
         String sql = "CREATE TABLE IF NOT EXISTS Shares (\n"
                 + "companyName VARCHAR(255) NOT NULL,\n"
                 + "companySymbol VARCHAR(255) NOT NULL,\n"
@@ -33,7 +38,11 @@ public class Database {
         return sql;
     }
     
-    public Connection connect() {
+    /**
+     * Creates a Connection object that connects to the database
+     * @return Connection
+     */
+    private Connection connect() {
         Connection conn = null;
         
         try {
@@ -49,26 +58,58 @@ public class Database {
         return conn;
     }
     
-    // testing
-    public static void main(String[] args) {
-        Database db = new Database();
-        Connection conn = null;
+    /**
+     * Selects all rows from the table Shares.
+     * Adds this data to a HashMap
+     * @return HashMap
+     */
+    public HashMap<Integer, HashMap<String, String>> getAllShares() {
+        String sql = "SELECT * FROM Shares";
+        
+        HashMap<Integer, HashMap<String, String>> shares = new HashMap<Integer, HashMap<String, String>>();
         
         try {
-            conn = db.connect();
-            String sql = db.createShareTableSQLStatement();
+            Connection conn = this.connect();
             Statement statement = conn.createStatement();
-            statement.execute(sql);
+            ResultSet rs = statement.executeQuery(sql);
+            
+            // used as an index for the hashmap.
+            // increments by 1 after going into next row in the query set
+            int count = 0;
+            
+            // check if result set is not empty
+            if (rs.next() != false) {
+                do {
+                    // loop through database and add data into a temp hashmap
+                    HashMap<String, String> share = new HashMap<>();
+                    share.put("companyName", rs.getString("companyName"));
+                    share.put("companySymbol", rs.getString("companySymbol"));
+                    share.put("available", rs.getString("availabe"));
+                    share.put("priceValue", rs.getString("priceValue"));
+                    share.put("priceCurrency", rs.getString("priceCurrency"));
+                    // add the temp hashmap into the main hashmap shares
+                    shares.put(count, share);
+                    count++;
+                } while (rs.next());
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
         }
+        
+        return shares;
+    }
+    
+
+    public static void main(String[] args) {
+        Database db = new Database();
+        HashMap<Integer, HashMap<String, String>> shares = db.getAllShares();
+        for (int i : shares.keySet()) {
+            HashMap<String, String> hm = shares.get(i);
+//            for (String s : hm.keySet()) {
+//                    System.out.println(s);
+//                    System.out.println(hm.get(s));
+//            }
+            System.out.println(hm.get("companyName"));
+	}
     }
 }
